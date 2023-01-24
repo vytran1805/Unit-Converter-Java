b310971687aa4124529c5bb5d1cc369b2f36662b
Vy Tran <vytran1805@gmail.com>
2023-01-24 17:52:58 -0500

    Created interfacce Converter that has abstract method convert()

---
+++ StrategyPattern-Lab1/src/pkgUnitConverter/Converter.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pkgUnitConverter;
+
+/**
+ *
+ * @author VyTran
+ */
+public interface Converter {
+    /**
+     * 
+     * @return 
+     */
+    public double convert(double unit);
+}
