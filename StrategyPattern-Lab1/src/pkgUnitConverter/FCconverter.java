5b0e909a3eb5245c7f5dd14f207a267e5ba58e58
Vy Tran <vytran1805@gmail.com>
2023-01-24 18:00:34 -0500

    This class implements method convert() to convert Fahrenheit to Celsius

--- StrategyPattern-Lab1/src/pkgUnitConverter/FCconverter.java
+++ StrategyPattern-Lab1/src/pkgUnitConverter/FCconverter.java
@@ -4,14 +4,16 @@ package pkgUnitConverter;
  * class to convert Fahrenheit to Celsius
  * @author kriger
  */
-public class FCconverter {
+public class FCconverter implements Converter{
 	private final double convFactor=1.8;
 	private final double convOrigin=32.0;
 	/**
 	 * @param fahrenheit value in Fahrenheit to be converted
 	 * @return equivalent in Celsius
 	 */
+        @Override
 	public double convert(double fahrenheit) {
 		return (fahrenheit - convOrigin)/convFactor;
 	}
+
 }
